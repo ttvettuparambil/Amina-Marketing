1a3eb4f122009dea96ca891d809377fe85c5e983
ttvettuparambil <70708447+ttvettuparambil@users.noreply.github.com>
2020-11-18 09:44:36 +0530

    Amina marketing sample website

---
+++ amina-marketing/assets/includes/blog.php
@@ -0,0 +1,140 @@
+<section class="latest-blog section-space pt-0">
+        <div class="container">
+          <div class="row">
+            <div class="col-12">
+              <div class="section-title text-center">
+                <h2>Latest Blogs</h2>
+                <p>
+                  Accumsan vitae pede lacus ut ullamcorper sollicitudin quisque
+                  libero
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-12">
+              <div class="blog-carousel-active slick-row-15 slick-arrow-style">
+                <!-- blog post item start -->
+                <div class="blog-post-item">
+                  <figure class="blog-thumb">
+                    <a href="blog-details.html">
+                      <img
+                        src="assets/img/blog/blog-img1.jpg"
+                        alt="blog image"
+                      />
+                    </a>
+                  </figure>
+                  <div class="blog-content">
+                    <h3 class="blog-title">
+                      <a href="blog-details.html"
+                        >Celebrity Daughter Opens Up About Having Her Eye Color
+                        Changed</a
+                      >
+                    </h3>
+                    <div class="blog-meta">
+                      <p>25/03/2019 | Debaco</p>
+                    </div>
+                  </div>
+                </div>
+                <!-- blog post item end -->
+
+                <!-- blog post item start -->
+                <div class="blog-post-item">
+                  <figure class="blog-thumb">
+                    <a href="blog-details.html">
+                      <img
+                        src="assets/img/blog/blog-img2.jpg"
+                        alt="blog image"
+                      />
+                    </a>
+                  </figure>
+                  <div class="blog-content">
+                    <h3 class="blog-title">
+                      <a href="blog-details.html"
+                        >Children Left Home Alone For 4 Days In TV series
+                        Experiment</a
+                      >
+                    </h3>
+                    <div class="blog-meta">
+                      <p>25/03/2019 | Debaco</p>
+                    </div>
+                  </div>
+                </div>
+                <!-- blog post item end -->
+
+                <!-- blog post item start -->
+                <div class="blog-post-item">
+                  <figure class="blog-thumb">
+                    <a href="blog-details.html">
+                      <img
+                        src="assets/img/blog/blog-img3.jpg"
+                        alt="blog image"
+                      />
+                    </a>
+                  </figure>
+                  <div class="blog-content">
+                    <h3 class="blog-title">
+                      <a href="blog-details.html"
+                        >Lotto Winner Offering Up Money To Any Man That Will
+                        Date Her</a
+                      >
+                    </h3>
+                    <div class="blog-meta">
+                      <p>25/03/2019 | Debaco</p>
+                    </div>
+                  </div>
+                </div>
+                <!-- blog post item end -->
+
+                <!-- blog post item start -->
+                <div class="blog-post-item">
+                  <figure class="blog-thumb">
+                    <a href="blog-details.html">
+                      <img
+                        src="assets/img/blog/blog-img4.jpg"
+                        alt="blog image"
+                      />
+                    </a>
+                  </figure>
+                  <div class="blog-content">
+                    <h3 class="blog-title">
+                      <a href="blog-details.html"
+                        >People are Willing to Lie When Comes Money, According
+                        to Research</a
+                      >
+                    </h3>
+                    <div class="blog-meta">
+                      <p>25/03/2019 | Debaco</p>
+                    </div>
+                  </div>
+                </div>
+                <!-- blog post item end -->
+
+                <!-- blog post item start -->
+                <div class="blog-post-item">
+                  <figure class="blog-thumb">
+                    <a href="blog-details.html">
+                      <img
+                        src="assets/img/blog/blog-img5.jpg"
+                        alt="blog image"
+                      />
+                    </a>
+                  </figure>
+                  <div class="blog-content">
+                    <h3 class="blog-title">
+                      <a href="blog-details.html"
+                        >Lesbian Love Stories Of Hollywood’s Biggest
+                        Celebrities</a
+                      >
+                    </h3>
+                    <div class="blog-meta">
+                      <p>25/03/2019 | Debaco</p>
+                    </div>
+                  </div>
+                </div>
+                <!-- blog post item end -->
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
